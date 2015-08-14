62bb8b351982a7949ea106d9643dc9a2997dbf68
Keshav Dasu <keshavdasu@yahoo.com>
2015-08-14 04:03:01 -0700

    Added segmented controller so the user can pick between playing with 2 cards or 3

--- MemMatch/MemMatch/ViewController.m
+++ MemMatch/MemMatch/ViewController.m
@@ -13,7 +13,11 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+- (IBAction)segmentedControlIndexChanged;
+
 @property (strong,nonatomic) CardMatchingGame * game;
+@property int numberOfCards;
 @end
 
 @implementation ViewController
@@ -21,7 +25,6 @@
 -(CardMatchingGame *)game{
     if(!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
                                                          usingDeck:[self createDeck]];
-    
     return _game;
 }
 
@@ -29,13 +32,33 @@
     return [[PlayingCardDeck alloc]init];
 }
 
+
+
+- (IBAction)segmentedControlIndexChanged {
+    switch (_segmentedControl.selectedSegmentIndex) {
+        case 0:
+            _numberOfCards = 2;
+            break;
+        case 1:
+            _numberOfCards = 3;
+            break;
+        default:
+            _numberOfCards = 2;
+            break;
+    }
+}
+
+
 - (IBAction)touchCardButton:(UIButton *)sender {
+    _segmentedControl.enabled = NO;
     NSUInteger chooseButtonIndex = [self.cardButtons indexOfObject:sender];
-    [self.game pickACardAtIndex:chooseButtonIndex numberOfCards:3]; //Working (insert the switch value here)
+    if(_numberOfCards < 2 || _numberOfCards > 3) _numberOfCards = 2;
+    [self.game chooseCardAtIndex:chooseButtonIndex numberOfCards:_numberOfCards];
     [self updateUI];
 }
 
 - (IBAction)reDealButton {
+    _segmentedControl.enabled = YES;
     for(UIButton * cardButton in self.cardButtons){
         [cardButton setBackgroundImage:[UIImage imageNamed:@"cardBack"] forState:UIControlStateNormal];
         [cardButton setTitle:@"" forState:UIControlStateNormal];
